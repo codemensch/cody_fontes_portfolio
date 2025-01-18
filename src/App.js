cb8b1fbc0479ab62953591cd7d74dc16eb13f9c9
Cody Fontes <codyfontes@Codys-MBP.lan>
2025-01-17 23:42:17 -0800

    Added assets folder and logo images for background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 import './App.css';
+import logo from './assets/images/cody-c.png';
 
 function App() {
   return (
     <>
+      <header className="header">
+        {/* <img src={logo} className="logo" alt="logo" /> */}
+      </header>
       <Main />
       <Footer />
     </>
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,5 @@
 import './Main.css'
-import Header from './header/Header'
+import Header from './header/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
